bc96492b03daee7431f88a2ce072755055eaf497
Madushan Gamage <madushanbsns@gmail.com>
2015-04-21 11:45:34 +0800

    2.1.6

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/madnik/ror-azure-demo.git'
 
 # setup rvm.
 set :rbenv_type, :user
-set :rbenv_ruby, '2.0.0-p451'
+set :rbenv_ruby, '2.1.6'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
